2494f598cfb1b24186f9a3489ee21ffd2c376a8a
Joshua Flanagan <joshuaflanagan@gmail.com>
2011-11-11 18:57:49 -0600

    Use Project#phases to list the phases for issues

--- lib/stint/project.rb
+++ lib/stint/project.rb
@@ -1,9 +1,19 @@
 module Stint
   class Project
     attr_accessor :repo
+    attr_writer :github
 
     def initialize(repo)
       @repo = repo
+    end
+
+    def github
+      @github ||= GitHub.new(@repo)
+    end
+
+    def phases
+      issue_names = github.labels.map{|l| l["name"]}
+      issue_names.select {|n| n =~ /^[0-9]/}.sort
     end
   end
 end
--- spec/stint/project_spec.rb
+++ spec/stint/project_spec.rb
@@ -1,8 +1,41 @@
-require 'stint/project'
+require 'spec_helper'
 
 describe Stint::Project do
+  before :each do
+    @project = Stint::Project.new 'githublogin/reponame'
+  end
+
   it "should be associated with a single repository" do
-    project = Stint::Project.new 'githublogin/reponame'
-    project.repo.should == 'githublogin/reponame'
+    @project.repo.should == 'githublogin/reponame'
+  end
+
+  describe "#phases" do
+    before :each do
+      github = double(Stint::GitHub.new(nil))
+      github.stub(:labels) do
+        [
+          {"name" => '2-Working'},
+          {"name" => '1 Waiting'},
+          {"name" => 'Negotiate'},
+          {"name" => '4Profit'}
+        ]
+      end
+      @project.github = github
+      @phases = @project.phases
+    end
+
+    it "should include all GitHub Issue labels prefixed with numbers" do
+      @phases.should include('1 Waiting')
+      @phases.should include('2-Working')
+      @phases.should include('4Profit')
+    end
+
+    it "should not include GitHub Issues without a numeric prefix" do
+      @phases.should_not include('Negotiate')
+    end
+
+    it "should sort the labels in numeric order" do
+      @phases.should == ['1 Waiting', '2-Working', '4Profit']
+    end
   end
 end
